c3f620e02e30a36100da6f217caa2b027d7d57d0
GATANINANDARKO <gataninandarko@students.amikom.ac.id>
2022-03-23 23:40:27 +0700

    Taxiapplication

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+            taxi.DriverName = "gatan";
+            taxi.OnDuty = false;
+            taxi.NumPasangger = 5;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPasangger();
+            taxi.DropOffPasangger();
+            Console.ReadKey();
+
+        }
+    
+     }
+}
